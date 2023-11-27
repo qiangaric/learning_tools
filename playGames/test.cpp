08e0c7e54223b827d0aa19c0b8df7a32b6eeacaa
qiangaric <gaojinq@126.com>
2023-11-27 11:30:24 +0800

    结构体测试

--- playGames/test.cpp
+++ playGames/test.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
+struct student
+{
+    string name;
+    int age;
+    int score;
+};
+
+void printStdeunt(const student *s)
+{
+    cout << "name:" << s->name << " age: " << s->age << "score:" << s->score;
+}
 int main(int argc, char *argv[])
 
 {
 
+    struct student s = {"zhangsan", 12, 90};
+    printStdeunt(&s);
     cout << "hello, world\n";
     return 0;
 }
